d6cf6bae7872fc4fca0129a3db9faa8cd85975a2
Otto Sipe <ottosipe@gmail.com>
2014-04-06 14:37:51 -0400

    working multipart form upload

--- backend/server/photos.js
+++ backend/server/photos.js
@@ -1,4 +1,8 @@
-var mongo = require("./database.js");
+var  mongo = require("./database.js"),
+     Busboy = require('busboy'),
+     fs = require('fs'),
+     path = require('path');
+
 
 /*
     /photos GET, POST endpoints
@@ -15,7 +19,40 @@ module.exports = function() {
 
     // POST /phots
     this.post = function(req, res) {
-        photos.insert(req.body, db_response(req, res));
+
+        var busboy = new Busboy({ headers: req.headers });
+
+        var uploaded_file;
+        busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+            console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding);
+
+            var saveTo = path.join(__dirname, "../tmp/", path.basename(fieldname));
+            file.pipe(fs.createWriteStream(saveTo));
+
+            // file.on('data', function(data) {
+            //     console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+            //     uploaded_file = data;
+            // });
+            //
+            // file.on('end', function() {
+            //     console.log('File [' + fieldname + '] Finished');
+            // });
+        });
+
+        var metadata = {};
+        busboy.on('field', function(key, val, valTruncated, keyTruncated) {
+            console.log('Field [' + key + ']: value: ' + val);
+            metadata[key] = val;
+        });
+
+        busboy.on('finish', function() {
+            console.log(metadata);
+            console.log(uploaded_file);
+            //photos.insert(req.body, db_response(req, res));
+            db_response(req, res)(null,{});
+        });
+
+        req.pipe(busboy);
     }
 
     function db_response(req, res) {
@@ -28,19 +65,3 @@ module.exports = function() {
         }
     }
 }
-
-
-
-
-
-/*
-// db test
-exports.db = function(req, res){
-    mongo.db.collection("test", function(err, collection){
-        collection.insert({ msg: "hello world" }, function(err, docs){
-            if(err) throw err
-            res.send(docs);
-        });
-    });
-};
-*/
--- backend/server/router.js
+++ backend/server/router.js
@@ -6,6 +6,10 @@ module.exports = function(app) {
 	mongo.connect(function(msg) {
 		if(msg == null) {
 
+			app.get("/", function(req,res) {
+				res.send('<html><body><form action="/photos" method="post" enctype="multipart/form-data"><input type="text" name="photoName"><input type="file" name="photo"></form></body></html>')
+			})
+
 			var photos = new Photos();
 			// define API photo here
 			app.get('/photos', photos.get);
