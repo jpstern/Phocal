bd0e134b300a63258fe5b2c0ba010307c3cbded1
Patrick Wilson <pdubslax@umich.edu>
2014-04-07 02:27:05 -0400

    added the heartView classes

---
+++ Phocal/likeView.h
@@ -0,0 +1,18 @@
+//
+//  likeView.h
+//  test
+//
+//  Created by Patrick Wilson on 4/6/14.
+//  Copyright (c) 2014 Patrick Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface likeView : UIView
+@property (nonatomic,strong) NSString * status;
+@property (nonatomic,strong) UIImageView *imgview;
+@property (nonatomic,strong) UILongPressGestureRecognizer *gestreg;
+@property (nonatomic,weak) id target;
+@property (nonatomic,assign) SEL selector;
+
+@end
