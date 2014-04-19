938ab2782926c172284b58cae3f91712dad486ce
Otto Sipe <ottosipe@gmail.com>
2014-04-19 19:40:49 -0400

    backend uses _id now, fixed photo parser

--- backend/server/photos.js
+++ backend/server/photos.js
@@ -19,7 +19,7 @@ module.exports = function() {
 
     var photos = mongo.db.collection("photos");
     photos.ensureIndex({ loc: "2dsphere" },{ background:true });
-    photos.ensureIndex({ id: 1 },{  background:true, unique:true, dropDups:true });
+    photos.ensureIndex({ _id: 1 },{  background:true, unique:true, dropDups:true });
 
     // GET /photos
     this.get = function(req, res, next){
@@ -44,22 +44,8 @@ module.exports = function() {
         if(!req.query.lng || !req.query.lat) {
             query = {};
         }
-        photos.find(query, {_id:0}).limit(20).sort({time:-1}).toArray(function(err, docs) {
-            console.log(docs)
-            for (var i in docs) {
-                var doc = docs[i];
-                if(!doc.votes) {
-                    doc.votes = [];
-                }
-                doc.didVote = contains(doc.votes, req.hash);
-                doc.votes = doc.votes.length;
-
-                doc.lat = doc.loc.coordinates[1] || 0;
-                doc.lng = doc.loc.coordinates[0] || 0;
-                delete doc.loc;
-            }
-            db_response(req, res, next)(err,docs);
-        });
+        photos.find(query).limit(20).sort({time:-1}).toArray(
+            db_response(req, res, next));
     };
 
     // POST /photos
@@ -86,7 +72,7 @@ module.exports = function() {
     // POST /photo/:id/hash (pass hash in body)
     this.post_vote = function(req, res, next) {
         if (!req.hash) throw new Error("Invalid Hash");
-        photos.update({id:req.params.id}, {$addToSet:{votes:req.hash}}, db_response(req, res, next));
+        photos.update({_id:req.params.id}, {$addToSet:{votes:req.hash}}, db_response(req, res, next));
     }
 
     // saves file to s3 and returns the hash
@@ -101,7 +87,7 @@ module.exports = function() {
             var exif = parser.parse();
 
             var metadata = {
-                id: file.hash,
+                _id: file.hash,
                 size: file.size,
                 type: file.headers["content-type"],
                 loc: {
@@ -115,13 +101,16 @@ module.exports = function() {
                 time: new Date()
             }
 
-            var put = s3.putBuffer(data, metadata.id, {
+            var put = s3.putBuffer(data, metadata._id, {
                 'Content-Length': metadata.size,
                 'Content-Type': metadata.type,
                 'x-amz-acl': 'public-read' // ensure public
             }, function(err, res) {
                 if (!err && 200 == res.statusCode) {
-                    photos.insert(metadata, callback);
+                    photos.insert(metadata, function(err, docs){
+                        if (err) callback(err);
+                        else callback(null, docs[0]);
+                    });
                 } else {
                     callback({"error": "Can't Save to S3"});
                 }
@@ -139,9 +128,33 @@ module.exports = function() {
     function db_response(req, res, next) {
         return function(err, docs) {
             if (err) next(err);
+
+
+                            console.log(docs);
+
+            for (var i in docs) {
+                var doc = docs[i];
+
+                doc = parsePhoto(doc, req);
+            }
             res.send(docs);
         }
     }
+}
+
+function parsePhoto(photo, req) {
+
+    if(!photo.votes) {
+        photo.votes = [];
+    }
+    photo.didVote = contains(photo.votes, req.hash);
+    photo.votes = photo.votes.length;
+
+    photo.lat = photo.loc.coordinates[1];
+    photo.lng = photo.loc.coordinates[0];
+    delete photo.loc;
+
+    return photo
 }
 
 // TODO: move to util...
