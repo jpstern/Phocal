fe2935001afa4e6af6778145bc7cdd22a12ff068
Otto Sipe <ottosipe@gmail.com>
2014-04-21 01:26:58 -0400

    fixed growing heart like

--- Phocal/LikeGestureView.m
+++ Phocal/LikeGestureView.m
@@ -26,44 +26,41 @@
     self.imgview = [[UIImageView alloc] initWithFrame:CGRectMake(self.frame.size.width/2-50, self.frame.size.height/2-50, 100, 100)];
     [self.imgview setImage:[UIImage imageNamed:@"fullHeart"]];
     [self addSubview:self.imgview];
-    self.imgview.transform = CGAffineTransformMakeScale(0.5f, 0.5f);
-    self.imgview.alpha = 0.0;
+    [self hideHeart];
     
     return self;
 }
 
-- (void)handleLongPress:(UIView *)bang{
-    if (self.gestreg.state==UIGestureRecognizerStateBegan){
-        [self aMethod];
+- (void)handleLongPress:(UIView *)bang {
+    if (self.gestreg.state==UIGestureRecognizerStateBegan) {
+        
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:1.4];
+        [UIView setAnimationDelegate:self];
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+        [UIView setAnimationDidStopSelector:@selector(afterAnimationStops)];
+        
+        [self.imgview setAlpha:1.0];
+        self.imgview.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
+        
+        [UIView commitAnimations];
+        
+        NSLog(@"BEGIN");
     }
 
     CGPoint coords = [self.gestreg locationInView:self];
     [self.imgview setFrame:CGRectMake(coords.x-50, coords.y-50, 100, 100)];
     
-    if (self.gestreg.state==UIGestureRecognizerStateEnded ) {
-        self.imgview.transform = CGAffineTransformMakeScale(0.5f, 0.5f);
-        self.imgview.alpha = 0.0;
+    if (self.gestreg.state==UIGestureRecognizerStateEnded && ![self.status isEqual:@"moving"]) {
+        [self hideHeart];
     }
 }
 
-- (void)aMethod{
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:1.4];
-    [UIView setAnimationDelegate:self];
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-    [UIView setAnimationDidStopSelector:@selector(afterAnimationStops)];
-    
-    [self.imgview setAlpha:1.0];
-    self.imgview.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
-    
-    [UIView commitAnimations];
-    
-}
-
--(void)afterAnimationStops{
+- (void)afterAnimationStops {
     NSLog(@"STATE: %ld", self.gestreg.state);
     
-    if (self.gestreg.state==UIGestureRecognizerStateCancelled) {
+    if (self.gestreg.state==UIGestureRecognizerStateCancelled ) {
+        [self hideHeart];
         return;
     }
     
@@ -71,7 +68,7 @@
     [UIView beginAnimations:nil context:nil];
     [UIView setAnimationDuration:0.5];
     [UIView setAnimationDelegate:self];
-    [UIView setAnimationDidStopSelector:@selector(finalstop)];
+    [UIView setAnimationDidStopSelector:@selector(hideHeart)];
     [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
     [self.imgview setAlpha:0.0];
     self.imgview.transform = CGAffineTransformMakeScale(4.0f, 4.0f);
@@ -80,8 +77,8 @@
     
 }
 
--(void)finalstop{
-    self.imgview.transform = CGAffineTransformMakeScale(0.5f, 0.5f);
+- (void)hideHeart {
+    self.imgview.transform = CGAffineTransformMakeScale(0.8f, 0.8f);
     self.imgview.alpha = 0.0;
     self.status=@"done";
 }
