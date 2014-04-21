5a0e251a2a3d8c1d51187a14c80664a99db65a0d
Josh Stern <jpstern@umich.edu>
2014-04-20 20:55:33 -0400

    blah

--- Phocal/CameraViewController.m
+++ Phocal/CameraViewController.m
@@ -174,6 +174,7 @@
     
     _previewLayer.frame = self.view.bounds;
     
+    
     AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
     if (device) {
